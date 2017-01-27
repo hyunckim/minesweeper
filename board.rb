046206748db252828895ee49fc45af3557d54609
hyunckim <hyunkim1209@gmail.com>
2017-01-26 17:31:45 -0800

    add methods

--- board.rb
+++ board.rb
@@ -1,3 +1,15 @@
 class Board
-  
+
+  def initialize
+    @grid = Array.new(9) { Array.new(9) }
+  end
+
+  def populate
+    @grid.each do |row|
+      row.each do |col|
+        @grid[row, col] = Tile.new
+      end
+    end
+  end
+
 end
